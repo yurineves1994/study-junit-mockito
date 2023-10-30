9763c98291c08d0550a63b56b30ec501e84407e8
Yuri Neves <yurineves1934@gmail.com>
2023-10-30 18:57:28 -0300

    feat: create test service -  deleteUser - Sucess

--- src/test/java/apijunit/services/impl/UserServiceImplTest.java
+++ src/test/java/apijunit/services/impl/UserServiceImplTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -80,8 +83,11 @@ public class UserServiceImplTest {
   }
 
   @Test
-  void delete() {
-
+  void deleteWithSuccess() {
+    when(repository.findById(anyInt())).thenReturn(optionalUser);
+    doNothing().when(repository).deleteById(anyInt());
+    service.delete(ID);
+    verify(repository,times(1)).deleteById(anyInt());
   }
 
   @Test
