ef67e066f3437ff3eb43de87a69b278411576326
Yuri Neves <yurineves1934@gmail.com>
2023-10-30 00:15:09 -0300

    feat: create controller Errors

---
+++ src/main/java/apijunit/controllers/exceptions/ExceptionsHandler.java
@@ -0,0 +1,24 @@
+package apijunit.controllers.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import apijunit.services.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ExceptionsHandler {
+
+  @ExceptionHandler(ObjectNotFoundException.class)
+  public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+    StandardError err = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), e.getMessage(),
+        request.getRequestURI());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+  }
+}
