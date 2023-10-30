fd6094d86c921646489c7f61e6dfaa73897195ac
Yuri Neves <yurineves1934@gmail.com>
2023-10-30 00:12:11 -0300

    feat: init project

---
+++ src/main/java/apijunit/ApijunitApplication.java
@@ -0,0 +1,13 @@
+package apijunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApijunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApijunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apijunit/ApijunitApplicationTests.java
@@ -0,0 +1,13 @@
+package apijunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
